ee74a94340d3cd254b4337cd8d78e203797ae9fb
MTJonathan <jonathan02330@gmail.com>
2024-07-23 16:35:48 -0500

    Mouse Follower

--- proyects/03-mouse-follower/src/App.jsx
+++ proyects/03-mouse-follower/src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from "react"
 import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({x:0, y:0})
+  useEffect(()=>{
+    const handleMove = (e) =>{
+      const {clientX, clientY} = e
+      setPosition({x:clientX, y:clientY})
+    }
+    if(enabled){
+      window.addEventListener('pointermove', handleMove)
+    }
 
+    //Se ejecuta cuando el componente se desmonta
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  },[enabled ])
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="follower" style={{transform: `translate(${position.x}px, ${position.y}px)`}}></div>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} Seguir Puntero
+      </button>
     </>
   )
 }
