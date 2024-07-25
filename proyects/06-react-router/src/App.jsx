1ab1df75b69b1e226d85d527bf0e09890bed4cff
MTJonathan <jonathan02330@gmail.com>
2024-07-25 16:50:36 -0500

    reactRoutes

--- proyects/06-react-router/src/App.jsx
+++ proyects/06-react-router/src/App.jsx
@@ -1,20 +1,67 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { Route, Routes, Link } from 'react-router-dom'
+import { Route, Routes, Link, useParams, Outlet, NavLink as NL } from 'react-router-dom'
 import './App.css'
 
 const Home = ()  => <h1>Home</h1>
-const SerchPage = () => <h1>SearchPage</h1>
+const SerchPage = () => {
+  const tacos = [
+    'cochinita',
+    'chili',
+    'carnita',
+    'quesadilla'
+  ]
+  return (
+    <div>
+      <h1>SearchPage</h1>
+      <ul>
+        {tacos.map(taco => (
+          <li key={taco}><Link to={`/tacos/${taco}`}>{taco}</Link></li>
+        ))} 
+      </ul>
+    </div>
+  )  
+} 
+const Tacos = () => {
+  const { nombreDelTaco } = useParams()
+  return (
+    <>
+      <h1>Tacos {nombreDelTaco}</h1>
+      <NavLink to={`details`}>Ir a los Detalles</NavLink>  
+      <Outlet/>
+    </>
+  )
+}
+const TacosDetails = () =>{
+  const { nombreDelTaco } = useParams()
+  return(
+    <div>
+      <h2>Detalles</h2>
+      <p>{nombreDelTaco}</p>
+    </div>
+  )
+}
+const NavLink = ({to, children, ...props}) => {
+  return(
+    <NL
+      {...props}
+      className={({isActive}) => isActive ? 'isactive' : ''}
+      to={to}
+    >
+      {children}
+    </NL>
+  )
+}
 function App() {
   return (
     <>
       <header>
-        <h1>Miduchollo 💵</h1>
+        <h1>React Router 🤖</h1>
         <nav>
           <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/search'>Search</Link></li>
+            <li><NavLink to='/'>Home</NavLink></li>
+            <li><NavLink to='/search'>Search</NavLink></li>
           </ul>
         </nav>
       </header>
@@ -22,6 +69,10 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/search' element={<SerchPage/>}/>
+          <Route path='/tacos/:nombreDelTaco' element={<Tacos/>}>
+            <Route path='details' element={<TacosDetails/>}/>
+          </Route>
+          <Route path='*' element={<h1>404</h1>}/>
         </Routes>
       </main>
     </>
