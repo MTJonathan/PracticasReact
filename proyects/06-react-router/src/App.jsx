3ead1cc03b2435b3d402fd3fc136b46f1e2ab06a
MTJonathan <jonathan02330@gmail.com>
2024-07-24 22:49:02 -0500

    reactRoutes

--- proyects/06-react-router/src/App.jsx
+++ proyects/06-react-router/src/App.jsx
@@ -1,33 +1,29 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Route, Routes, Link } from 'react-router-dom'
 import './App.css'
 
+const Home = ()  => <h1>Home</h1>
+const SerchPage = () => <h1>SearchPage</h1>
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1>Miduchollo 💵</h1>
+        <nav>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/search'>Search</Link></li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/search' element={<SerchPage/>}/>
+        </Routes>
+      </main>
     </>
   )
 }
